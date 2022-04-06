f07fae22d4aa6e3d2ca1204f573d615edb16e711
Haohang Zhou <zhouhaohangau@gmail.com>
2022-04-06 21:00:33 +1000

    Added Routes and Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,35 +1,18 @@
 import React from 'react';
-import Directory from './components/Directory';
+import { Route, Routes } from 'react-router-dom';
+import NavBar from './components/NavBar';
+import Home from './routes/Home';
+import SignIn from './routes/SignIn';
 
 const App = () => {
-	const categories = [
-		{
-			id: 1,
-			title: 'hats',
-			imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-		},
-		{
-			id: 2,
-			title: 'jackets',
-			imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-		},
-		{
-			id: 3,
-			title: 'sneakers',
-			imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-		},
-		{
-			id: 4,
-			title: 'womens',
-			imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-		},
-		{
-			id: 5,
-			title: 'mens',
-			imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-		},
-	];
-	return <Directory categories={categories} />;
+	return (
+		<Routes>
+			<Route path='/' element={<NavBar />}>
+				<Route index element={<Home />} />
+				<Route path='sign-in' element={<SignIn />} />
+			</Route>
+		</Routes>
+	);
 };
 
 export default App;
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React, { Fragment } from 'react';
+
+import { Link, Outlet } from 'react-router-dom';
+import { ReactComponent as Logo } from '../assets/crown.svg';
+import './NavBar.scss';
+const NavBar = () => {
+	return (
+		<>
+			<div className='navigation'>
+				<Link className='logo-container' to='/'>
+					<Logo className='logo' />
+				</Link>
+				<div className='nav-links-container'>
+					<div className='nav-link '>
+						<Link to='shop'>Shop</Link>
+					</div>
+					<div className='nav-link '>
+						<Link to='sign-in'>Sign In</Link>
+					</div>
+				</div>
+			</div>
+
+			<Outlet />
+		</>
+	);
+};
+
+export default NavBar;
