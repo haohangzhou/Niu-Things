20d6eadbb8038984310733c0b4c0632183c75922
Haohang Zhou <zhouhaohangau@gmail.com>
2022-05-24 17:27:29 +1000

    Revert "Added Redux-Dev-Tool"
    This reverts commit 2005c271ba52d74d3189953fc080d855490f2ba5.

--- src/store/store.js
+++ src/store/store.js
@@ -3,16 +3,8 @@ import logger from 'redux-logger';
 
 import { rootReducer } from './root-reducer';
 
-// eslint-disable-next-line no-undef
-const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
-	Boolean
-);
-const composeEnhancer =
-	(process.env.NODE_ENV !== 'production' &&
-		window &&
-		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-	compose;
+const middleWares = [logger];
 
-const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 export const store = createStore(rootReducer, composedEnhancers);
